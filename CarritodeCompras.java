a5493af3a27a40c784adc02fa77495b5cfa58328
Tomás <tomasgonza64@gmail.com>
2023-04-20 19:49:46 -0300

    Carrito con Exceptios agregadas

--- CarritodeCompras.java
+++ CarritodeCompras.java
@@ -1,16 +1,22 @@
 package practicacarrito;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class CarritodeCompras {	
 	
 
 	public static void main(String[] args) {
-		
 	
 		Scanner teclado = new Scanner(System.in);
+		boolean continua;
+		
+		do {
+		try {
+	
+		continua = false;
 		System.out.println("Ingrese que producto desea agregar al Carrito:");
-		System.out.println("______________________________________________");
+		System.out.println("_____________________________________________________________________");
 		System.out.println("Seleccionar: ");
 		String NombreProducto1 = teclado.next();
 		
@@ -29,7 +35,7 @@ public class CarritodeCompras {
 		//SEGUNDO PRODUCTO
 		
 		System.out.println("Ingrese que producto desea agregar al Carrito:");
-		System.out.println("______________________________________________");
+		System.out.println("_____________________________________________________________________");
 		System.out.println("Seleccionar: ");
 		String NombreProducto2 = teclado.next();
 		
@@ -44,11 +50,11 @@ public class CarritodeCompras {
 		
 		ItemsCarrito item2 = new ItemsCarrito(compra2.NombreProducto , compra2.CantidadProducto , compra2.PrecioProducto , compra2.CantidadProducto*compra2.PrecioProducto);
 		
-		
+	
 		//TERCER PRODUCTO
 		
 		System.out.println("Ingrese que producto desea agregar al Carrito:");
-		System.out.println("______________________________________________");
+		System.out.println("_____________________________________________________________________");
 		System.out.println("Seleccionar: ");
 		String NombreProducto3 = teclado.next();
 		
@@ -63,29 +69,29 @@ public class CarritodeCompras {
 		
 		ItemsCarrito item3 = new ItemsCarrito(compra3.NombreProducto , compra3.CantidadProducto , compra3.PrecioProducto , compra3.CantidadProducto*compra3.PrecioProducto);
 				
-		
+	
 		//TOTAL PRECIO   ITEM 1+2+3
 		
-		System.out.println("_______________________________________________");
+		System.out.println("_____________________________________________________________________");
 		System.out.println("CARRITO DE COMPRAS - TIENDA VIRTUAL");
-		System.out.println("_______________________________________________");
+		System.out.println("_____________________________________________________________________");
 		System.out.println("Producto: " + item.NombreProducto);
 		System.out.println("Precio Unitario: $" + item.PrecioProducto);
 		System.out.println("Cantidad: " + item.CantidadProducto + " Unid.");
 		System.out.println("Precio: $" + item.PrecioItemsCarrito);
-		System.out.println("_______________________________________________");
+		System.out.println("_____________________________________________________________________");
 		
 		System.out.println("Producto: " + item2.NombreProducto);
 		System.out.println("Precio Unitario: $" + item2.PrecioProducto);
 		System.out.println("Cantidad: " + item2.CantidadProducto + " Unid.");
 		System.out.println("Precio: $" + item2.PrecioItemsCarrito);
-		System.out.println("_______________________________________________");
+		System.out.println("_____________________________________________________________________");
 		
 		System.out.println("Producto: " + item3.NombreProducto);
 		System.out.println("Precio Unitario: $" + item3.PrecioProducto);
 		System.out.println("Cantidad: " + item3.CantidadProducto + " Unid.");
 		System.out.println("Precio: $" + item3.PrecioItemsCarrito);
-		System.out.println("_______________________________________________");
+		System.out.println("_____________________________________________________________________");
 		
 		
 		if(item.PrecioItemsCarrito + item2.PrecioItemsCarrito + item3.PrecioItemsCarrito > 1000) {
@@ -105,7 +111,7 @@ public class CarritodeCompras {
 			
 			System.out.println("Precio Carrito: $" + (item.PrecioItemsCarrito + item2.PrecioItemsCarrito + item3.PrecioItemsCarrito));
 			System.out.println("Descuento Aplicado del 10%: $" + (item.PrecioItemsCarrito + item2.PrecioItemsCarrito + item3.PrecioItemsCarrito)*0.10);
-			System.out.println("_______________________________________________");
+			System.out.println("_____________________________________________________________________");
 			System.out.println("         Total a Pagar con Descuento: $" + total);
 						
 		}
@@ -115,12 +121,21 @@ public class CarritodeCompras {
 				
 				System.out.println("Precio Carrito: $" + (item.PrecioItemsCarrito + item2.PrecioItemsCarrito + item3.PrecioItemsCarrito));
 				System.out.println("No se encontraron Descuentos para Aplicar.");
-				System.out.println("_______________________________________________");
+				System.out.println("_____________________________________________________________________");
 				System.out.println("                       Total a Pagar: " + (item.PrecioItemsCarrito + item2.PrecioItemsCarrito + item3.PrecioItemsCarrito));
 			}
+		}
+		catch(InputMismatchException ex) {
+			System.out.println("_____________________________________________________________________");
+			System.out.println("Hubo un Error al Introducir los Datos... Por favor intente nuevamente: ");
+			 teclado.next();
+             continua = true;
+         }
+     } while (continua);
 		}
 	
 
 	}
+
 
 
